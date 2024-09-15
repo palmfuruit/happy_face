92993d2f666e1f70daf0ba708092f767ec4a1331
Palm <palmfuruit46@gmail.com>
2024-09-15 20:41:12 +0900

    本番環境向けに、importを相対パスに変更。

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
-from face_detection import detect_faces, align_face
-from emotion_analysis import analyze_emotions
+from .face_detection import detect_faces, align_face
+from .emotion_analysis import analyze_emotions
 
 # Flask のインスタンスを作成
 app = Flask(__name__)
