8c3fdc9da38ce961151169d2b468bf8343a46465
Palm <palmfuruit46@gmail.com>
2024-09-17 20:47:53 +0900

    前処理に正規化を追加。

--- src/emotion_analysis_nolib.py
+++ src/emotion_analysis_nolib.py
@@ -4,16 +4,13 @@ import tensorflow as tf
 import torch
 from transformers import AutoModelForImageClassification
 import torch.nn.functional as F
-# import os
 
-# FERライブラリで使用されている感情認識モデルのロード
-# モデルファイルは事前に取得しておく（例: fer_model.h5）
 
 from transformers import AutoModel, AutoTokenizer
 
+# https://huggingface.co/motheecreator/vit-Facial-Expression-Recognition
 model_name = "motheecreator/vit-Facial-Expression-Recognition"
 model = AutoModelForImageClassification.from_pretrained(model_name)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 # クラスラベルの定義（FERライブラリのクラスラベルに合わせる）
@@ -28,9 +25,15 @@ def preprocess_input(face_image):
     if face_image.shape[-1] == 1:
         face_image = np.repeat(face_image, 3, axis=-1)  # 3チャンネルに複製
     
+    # 正規化: 平均0.5、標準偏差0.5に基づいて正規化
+    face_image = (face_image - 0.5) / 0.5
+    
     # PyTorch用に [batch_size, channels, height, width] へ変換
     face_image = np.transpose(face_image, (2, 0, 1))  # チャンネルを先頭に移動
     face_image = np.expand_dims(face_image, axis=0)  # バッチ次元を追加
+    
+    # NumPyからPyTorchテンソルへ変換
+    face_image = torch.tensor(face_image).to(torch.float32)
     
     return face_image
 
@@ -39,17 +42,14 @@ def analyze_emotions(aligned_face_cropped):
     # 画像を前処理
     processed_face = preprocess_input(aligned_face_cropped)
     
-    # NumPy配列をPyTorchテンソルに変換
-    processed_face_tensor = torch.tensor(processed_face).to(torch.float32)
-    
     # デバイスの設定（CPUまたはGPU）
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    processed_face_tensor = processed_face_tensor.to(device)
+    processed_face = processed_face.to(device)
     model.to(device)
     
     # モデルで推論を実行
     with torch.no_grad():  # 勾配を計算しないようにする
-        predictions = model(processed_face_tensor)
+        predictions = model(processed_face)
     
     # 感情スコアを取得し、softmaxで確率に変換
     emotion_scores = predictions.logits[0]  # logitsを使用
